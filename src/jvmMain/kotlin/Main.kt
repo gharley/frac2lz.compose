3f068d14ace987949dbaf77208d3f9b7ab65cba8
Greg Harley <greg@harleyhood.com>
2023-01-16 14:06:35 -0700

    moved MainMenu to separate file

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -77,37 +77,7 @@ fun App() {
         var text by remember { mutableStateOf("Hello, World!") }
 
         MaterialTheme {
-            MenuBar {
-                Menu("File") {
-                    Item("Open Fractal", onClick = { EventBus.publish(FileEvent(FileAction.OPEN_FRACTAL)) })
-                    Item("Open JSON file", onClick = { EventBus.publish(FileEvent(FileAction.OPEN_JSON)) })
-                    Separator()
-                    Item("Open Palette", onClick = { EventBus.publish(FileEvent(FileAction.OPEN_PALETTE)) })
-                    Separator()
-                    Item("Save Fractal", onClick = { EventBus.publish(FileEvent(FileAction.SAVE_FRACTAL)) })
-                    Item("Save JSON file", onClick = { EventBus.publish(FileEvent(FileAction.SAVE_JSON)) })
-                    Separator()
-                    Item("Save Palette", onClick = { EventBus.publish(FileEvent(FileAction.SAVE_PALETTE)) })
-                    Separator()
-                    Item("Export to image file", onClick = { EventBus.publish(FileEvent(FileAction.SAVE_IMAGE)) })
-                    Separator()
-                    Item("Exit", onClick = { exitProcess(0) })
-                }
-                Menu("Fractal") {
-                    Item(
-                        "Calculate Base Fractal",
-                        onClick = { EventBus.publish(CalculateEvent(CalculateAction.CALCULATE_BASE)) })
-                    Separator()
-                    Item("Recalculate", onClick = { EventBus.publish(CalculateEvent(CalculateAction.RECALCULATE)) })
-                    Item("Refine", onClick = { EventBus.publish(CalculateEvent(CalculateAction.REFINE)) })
-                    Separator()
-                    Item("Refresh Image", onClick = { EventBus.publish(CalculateEvent(CalculateAction.REFRESH)) })
-                    Separator()
-                    Item(
-                        "Show Histogram",
-                        onClick = { EventBus.publish(CalculateEvent(CalculateAction.SHOW_HISTOGRAM)) })
-                }
-            }
+            MainMenu()
             Column {
                 Button(onClick = {
                     text = "Hello, Desktop!"
