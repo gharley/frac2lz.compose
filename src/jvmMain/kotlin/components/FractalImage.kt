489f87748c736b09dc416e6a53f5c892234160fe
Greg Harley <greg@harleyhood.com>
2023-01-21 19:21:11 -0700

    working on image canvas

--- src/jvmMain/kotlin/components/FractalImage.kt
+++ src/jvmMain/kotlin/components/FractalImage.kt
@@ -1,25 +1,21 @@
 package components
 
-import EventBus
 import Palette
-import action.FractalEvent
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.SwingPanel
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asSkiaBitmap
-import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
 import state.FractalParameters
-import kotlin.math.min
+import javax.swing.BoxLayout
+import javax.swing.JPanel
 
 @Composable
 fun FractalImage(params: FractalParameters, palette: Palette) {
@@ -27,65 +23,45 @@ fun FractalImage(params: FractalParameters, palette: Palette) {
     var parentBounds = remember { Size.Zero }
     val fractalHeight = parameters.height
     val fractalWidth = parameters.width
+    val swingImage = SwingImage(params, palette)
 
     Surface(Modifier.onGloballyPositioned { coords -> parentBounds = coords.size.toSize() }, elevation = 5.dp) {
-        Canvas(
-            Modifier.size(width = fractalWidth.dp, height = fractalHeight.dp)
-                .background(color = Color.Gray).onGloballyPositioned { coords -> parentBounds = coords.size.toSize() }
-        ) {
-            var scaleValue =
-                when {
-                    size.width > size.height -> when {
-                        parentBounds.width > parentBounds.height -> min(parentBounds.width / fractalWidth.toFloat(), parentBounds.height / fractalHeight.toFloat())
-                        else -> parentBounds.width / fractalWidth.toFloat()
-                    }
-                    fractalWidth < fractalHeight -> when {
-                        parentBounds.width > parentBounds.height -> parentBounds.height / fractalHeight.toFloat()
-                        else -> 1.0F
-                    }
-                    else -> min(parentBounds.width / fractalWidth.toFloat(), parentBounds.height / fractalHeight.toFloat())
+        SwingPanel(modifier = Modifier
+            .size(width = fractalWidth.dp, height = fractalHeight.dp)
+            .background(color = Color.Gray).onGloballyPositioned { coords -> parentBounds = coords.size.toSize() },
+            factory = {
+                JPanel().apply {
+                    layout = BoxLayout(this, BoxLayout.Y_AXIS)
+                    add(swingImage)
                 }
-
-            scale(scaleValue){}
-
-            val pixels = ByteArray(parameters.width.toInt()*4)
-            var image = ImageBitmap(fractalWidth.toInt(), fractalHeight.toInt())
-//            val bitmap = Bitmap()
-
-            EventBus.listen(FractalEvent::class.java).subscribe { event ->
-                val color = palette.color(event.data)
-                val column = event.column * 4
-                pixels[column] = (color.red * 255).toInt().toByte()
-                pixels[column+1] = (color.green * 255).toInt().toByte()
-                pixels[column+2] = (color.blue * 255).toInt().toByte()
-                pixels[column+3] = (color.alpha * 255).toInt().toByte()
-
-                if (event.endOfRow) {
-                    image.asSkiaBitmap().installPixels(pixels)
-                    drawImage(image)
-//                    drawIntoCanvas {
-//                        bitmap.installPixels(ImageInfo(10,10,ColorType.RGBA_8888,ColorAlphaType.OPAQUE))
-//                        it.nativeCanvas.writePixels(bitmap, 0, event.row)
-//                        it.nativeCanvas
-//                    }
-                }
+            },
+            update = {
+                swingImage.prepareForCalc(fractalWidth, fractalHeight)
             }
+        )
+//            val scaleValue =
+//                when {
+//                    size.width > size.height -> when {
+//                        parentBounds.width > parentBounds.height -> min(
+//                            parentBounds.width / fractalWidth.toFloat(),
+//                            parentBounds.height / fractalHeight.toFloat()
+//                        )
 //
-//            if (rowList.isNotEmpty()) {
-////                lock.lock()
-//                try {
-//                    val writer = graphicsContext2D.pixelWriter
-//
-//                    rowList.forEach { (k, v) ->
-//                        writer.setPixels(0, k, size.width.toInt(), 1, PixelFormat.getByteBgraInstance(), v, 0, v.size)
+//                        else -> parentBounds.width / fractalWidth.toFloat()
 //                    }
 //
-//                    rowList.clear()
-//                } finally {
-////                    lock.unlock()
+//                    fractalWidth < fractalHeight -> when {
+//                        parentBounds.width > parentBounds.height -> parentBounds.height / fractalHeight.toFloat()
+//                        else -> 1.0F
+//                    }
+//
+//                    else -> min(
+//                        parentBounds.width / fractalWidth.toFloat(),
+//                        parentBounds.height / fractalHeight.toFloat()
+//                    )
 //                }
-//            }
-//        }
-        }
+//
+//            scale(scaleValue) {}
+
     }
 }
---
+++ src/jvmMain/kotlin/components/SwingImage.kt
@@ -0,0 +1,104 @@
+package components
+
+import Palette
+import action.FractalEvent
+import androidx.compose.ui.graphics.toArgb
+import state.FractalParameters
+import java.awt.Canvas
+import java.awt.Graphics
+import java.awt.Graphics2D
+import java.awt.Image
+import java.awt.image.MemoryImageSource
+import java.io.File
+import java.util.concurrent.locks.ReentrantLock
+import javax.swing.JComponent
+import javax.swing.JPanel
+
+class SwingImage(params: FractalParameters, palette: Palette) : Canvas() {
+    private val height = params.height.toInt()
+    private val width = params.width.toInt()
+    private var pixels = IntArray(width * height)
+    private val source = MemoryImageSource(width, height, pixels, 0, width)
+    private var image = createImage(source)
+
+    init {
+//        add(ZoomBox(this))
+
+        EventBus.listen(FractalEvent::class.java).subscribe {
+            val column = it.column
+            val color = palette.color(it.data)
+
+            pixels[it.row * width + column] = color.toArgb()
+//            val offset = column * 4
+//
+//            this@SwingImage.buffer[offset] = color.red.toInt().toByte()
+//            this@SwingImage.buffer[offset + 1] = color.green.toInt().toByte()
+//            this@SwingImage.buffer[offset + 2] = color.blue.toInt().toByte()
+//            this@SwingImage.buffer[offset + 3] = color.alpha.toInt().toByte()
+
+            if (it.endOfRow) {
+                source.newPixels(0, it.row, width, 1)
+            }
+        }
+    }
+
+    override fun paint(g: Graphics?) {
+        val graphics = g as Graphics2D
+
+        graphics.drawImage(image, null, null)
+    }
+
+    fun prepareForCalc(fractalWidth: Double, fractalHeight: Double, clear: Boolean = true) {
+        pixels.fill(0xd3d3d3ff.toInt())
+        source.newPixels(0, 0, width, height)
+//        with(canvas) {
+//            if (clear) graphicsContext2D.clearRect(0.0, 0.0, width, height)
+//
+//            translateX = 0.0
+//            translateY = 0.0
+//            scaleX = 1.0
+//            scaleY = 1.0
+//
+//            val parentBounds = parent.layoutBounds
+//
+//            = fractalWidth.toInt()
+//            height = fractalHeight.toInt()
+//
+//            val scale: Double =
+//                when {
+//                    fractalWidth > fractalHeight -> when {
+//                        parentBounds.width > parentBounds.height -> min(
+//                            parentBounds.width / fractalWidth,
+//                            parentBounds.height / fractalHeight
+//                        )
+//
+//                        else -> parentBounds.width / fractalWidth
+//                    }
+//
+//                    fractalWidth < fractalHeight -> when {
+//                        parentBounds.width > parentBounds.height -> parentBounds.height / fractalHeight
+//                        else -> 1.0
+//                    }
+//
+//                    else -> min(parentBounds.width / fractalWidth, parentBounds.height / fractalHeight)
+//                }
+//
+//            scaleX = scale
+//            scaleY = scale
+//            translateX = -boundsInParent.minX
+//            translateY = -boundsInParent.minY
+//
+//            this@SwingImage.buffer = ByteArray((fractalWidth * 4).toInt())
+//        }
+    }
+
+//        fun saveToImageFile(file: File, fileType: String = "png") {
+//            val writableImage = WritableImage(canvas.width.toInt(), canvas.height.toInt())
+//            val snapshotParams = SnapshotParameters()
+//
+//            snapshotParams.transform = Transform.scale(1.0 / canvas.scaleX, 1.0 / canvas.scaleY)
+//            canvas.snapshot(snapshotParams, writableImage)
+//            val renderedImage: java.awt.image.RenderedImage = javafx.embed.swing.SwingFXUtils.fromFXImage(writableImage, null)
+//            javax.imageio.ImageIO.write(renderedImage, fileType, file)
+//        }
+}
