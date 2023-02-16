26bccee21b2639b888ec84b120ecfbc6b33cb887
Greg Harley <greg@harleyhood.com>
2023-02-16 09:57:09 -0700

    upgraded compose and Material3

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
         withJava()
     }
     sourceSets {
-        val material3Version = "1.2.2"
+        val material3Version = "1.3.0"
         val rxVersion = "3.1.3"
         val jvmMain by getting {
             dependencies {
